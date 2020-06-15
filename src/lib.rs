0358a46ce75e174d4fd093c63986b91d4266324d
jlkiri <interalia.dev@gmail.com>
2020-06-15 23:36:05 +0900

    Implement stack

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,43 @@
-struct Stack {
-  length: usize,
+struct Stack<T> {
+  stack: Vec<T>,
 }
 
-impl Stack {
-  fn new() -> Stack {
-    Stack { length: 0 }
+impl<T> Stack<T> {
+  fn new() -> Stack<T> {
+    Stack { stack: Vec::new() }
+  }
+
+  fn length(&self) -> usize {
+    self.stack.len()
+  }
+
+  fn pop(&mut self) -> Option<T> {
+    self.stack.pop()
+  }
+
+  fn push(&mut self, item: T) {
+    self.stack.push(item)
+  }
+
+  fn peek(&self) -> Option<&T> {
+    self.stack.get(self.length() - 1)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn stack_peek() {
+    let mut stack: Stack<isize> = Stack::new();
+    stack.push(1);
+    assert_eq!(*stack.peek().unwrap(), 1);
+  }
+
+  #[test]
+  #[should_panic]
+  fn stack_panic() {
+    let mut stack: Stack<isize> = Stack::new();
+    let last_element = stack.peek().unwrap();
   }
 }
